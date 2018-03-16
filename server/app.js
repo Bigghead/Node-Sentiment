f3099027d4ef702201e053e8744d7627ab176a35
Bigghead <shavenchew@yahoo.com>
2018-03-16 14:55:12 -0700

    returning promise from fs

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bodyParser = require( 'body-parser' );
 const fs         = require( 'fs' );
 const Clarifai   = require( 'clarifai' );
 const app        = express();
+const { readFile } = require( './helpers/fsHelper' );
 
 const clarifai = new Clarifai.App( { apiKey: process.env.CLARIFAI_KEY } );
 
@@ -13,21 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get( '/', ( req, res ) => {
-    fs.readFile('./Rottweiler-1.jpg', function(err, original_data){
-        if(err){
-            return;
-        }
-        else{
-            // this variable contains the correctly encoded image. Just use it as it is in the Clarifai API
-            var base64Image = original_data.toString('base64');
-            clarifai.models.predict( Clarifai.GENERAL_MODEL, base64Image )
+    
+
+    readFile( './Rottweiler-1.jpg' )
+        .then( data => {
+            let baseImage = data.toString('base64')
+            clarifai.models.predict( Clarifai.GENERAL_MODEL, baseImage )
                     .then( 
                         result => res.json( result ),
-                        err => res.json(err)
+                        err => {
+                            throw new Error( err )
+                        }
                     )
-        }
-     });
-    
+        } ) 
+        .catch( error => res.status(400).json( { error } ) )
 } )
 
 app.listen( 9000 )
---
+++ server/helpers/fsHelper.js
@@ -0,0 +1,16 @@
+const fs = require( 'fs' );
+
+
+const readFile = ( filePath ) => {
+    return new Promise( ( resolve, reject ) => {
+        fs.readFile( filePath, ( err, data ) =>  {
+            if( err ) reject( err );
+            resolve( data );
+        } )
+    } )
+}
+
+
+module.exports = {
+    readFile
+}
